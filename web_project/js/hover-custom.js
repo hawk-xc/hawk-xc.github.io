0a5f712eb8837d5822ba866b7ca6ee30696636ee
wahyu_bsi_acc <12221171@bsi.ac.id>
2023-04-08 12:44:21 +0700

    add update

---
+++ web_project/js/hover-custom.js
@@ -0,0 +1,7 @@
+// custom js
+const navbarNav = document.querySelector(".navbar-icon-link-hover");
+
+function MyFunction() {
+  const navshow = document.querySelector(".navbar-icon-link-hover");
+  navshow.classList.toggle("active");
+}
