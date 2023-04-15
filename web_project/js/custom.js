1c57672f09e81943ed424e28f8d96010c5380a07
wahyu tri <hawkxc068@gmail.com>
2023-04-16 02:48:21 +0700

    add update

---
+++ web_project/js/custom.js
@@ -0,0 +1,11 @@
+function copyURI(evt) {
+  evt.preventDefault();
+  navigator.clipboard.writeText(evt.target.getAttribute("href")).then(
+    () => {
+      /* clipboard successfully set */
+    },
+    () => {
+      /* clipboard write failed */
+    }
+  );
+}
