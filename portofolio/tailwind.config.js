3b08ee88efe707516b57db0f54d1d86f13fe6a2c
wahyu tri <hawkxc068@gmail.com>
2023-10-01 21:39:40 +0700

    add portofolio #tailwindcss project

---
+++ portofolio/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Views/**/*.{html, js}"],
+  darkMode: ['class'],
+  theme: {
+    extend: {
+      fontFamily: {
+        VarelaRound: ['Varela Round', 'sans-serif'],
+        CartographCF: 'Cartograph CF',
+      },
+      spacing: {
+        13: '7rem'
+      },
+
+      animation: {
+        munculkan: 'muncul_pelan 0.3s ease-in',
+      },
+
+      keyframes: {
+        muncul_pelan: {
+          'from': { opacity: '0' },
+          'to': { opacity: '1' },
+        },
+      },
+
+      backgroundColor: {
+        rada_hitam: 'rgb(0 0 0 / .1)',
+      },
+    },
+  },
+  plugins: [],
+}
+
