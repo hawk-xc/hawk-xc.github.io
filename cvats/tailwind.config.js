a7b91c574e1946e8e9c4d0d8f9287629c082ecac
wahyu tri <hawkxc068@gmail.com>
2023-10-03 12:33:19 +0700

    add new update

--- cvats/tailwind.config.js
+++ cvats/tailwind.config.js
@@ -1,33 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Views/**/*.{html, js}"],
-  darkMode: ['class'],
+  content: ["./**/*.{html, js}"],
+  darkMode: ["class"],
   theme: {
     extend: {
       fontFamily: {
-        VarelaRound: ['Varela Round', 'sans-serif'],
-        CartographCF: 'Cartograph CF',
+        VarelaRound: ["Varela Round", "sans-serif"],
+        CartographCF: "Cartograph CF",
       },
       spacing: {
-        13: '7rem'
+        13: "7rem",
       },
 
       animation: {
-        munculkan: 'muncul_pelan 0.3s ease-in',
+        munculkan: "muncul_pelan 0.3s ease-in",
       },
 
       keyframes: {
         muncul_pelan: {
-          'from': { opacity: '0' },
-          'to': { opacity: '1' },
+          from: { opacity: "0" },
+          to: { opacity: "1" },
         },
       },
 
       backgroundColor: {
-        rada_hitam: 'rgb(0 0 0 / .1)',
+        rada_hitam: "rgb(0 0 0 / .1)",
       },
     },
   },
   plugins: [],
-}
-
+};
